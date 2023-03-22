e18e8a3222b32f89a1c9562684b089ff5bae6ac9
Mouaad Kharchich <mkharchich@smb.sqli.com>
2023-03-22 16:38:54 +0000

    add count tag helpers

---
+++ Formation/TagHelpers/CountRestaurants.cs
@@ -0,0 +1,32 @@
+﻿using Formation.Abstructs;
+using Formation.Services;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Formation.TagHelpers
+{
+
+    [HtmlTargetElement("count")]
+    public class CountRestaurants : TagHelper
+    {
+        private readonly IRestaurantService restaurantService;
+
+        public CountRestaurants(IRestaurantService restaurantService)
+        {
+            this.restaurantService = restaurantService;
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "count";
+            int count = restaurantService.GetRestaurants().Count;
+            if (count != 0)
+            {
+                output.Content.SetContent($"Count : {count}");
+            }
+            else
+            {
+                output.Content.SetContent($"Vide!");
+            }
+        }
+    }
+}
